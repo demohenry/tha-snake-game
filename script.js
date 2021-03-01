a4d9c3dc0d332dcdd1af2106d40ff417fe7e7a38
Democrito <demohenry6@gmail.com>
2021-03-01 17:30:07 -0300

    função desenhar comida

--- script.js
+++ script.js
@@ -9,6 +9,11 @@ snake[0] = {
 
 let direction = 'right';
 
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
+
 function createBG() {
 	context.fillStyle = 'lightgreen';
 	context.fillRect(0, 0, 16 * box, 16 * box);
@@ -20,6 +25,14 @@ function createSnake() {
 		context.fillRect(snake[i].x, snake[i].y, box, box);
 	}
 }
+
+function drawFood(){
+  context.fillStyle = "red";
+  context.fillRect(food.x, food.y, box, box);
+}
+
+
+
 
 document.addEventListener('keydown', update)
 
@@ -38,9 +51,9 @@ function initGame() {
   if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0
   if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
 
-  
 	createBG();
   createSnake();
+  drawFood()
   
   let snakeX = snake[0].x;
   let snakeY = snake[0].y;
